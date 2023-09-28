5f339a60d88595bb4f37d78ec417eab2b8b27e8c
Guy Baudouin <guythoun@gmail.com>
2023-09-28 19:08:07 +0200

    fix discard (A8_LaManAPyramid)

--- modules/php/Actions/Discard.php
+++ modules/php/Actions/Discard.php
@@ -74,7 +74,7 @@ class Discard extends \AK\Models\Action
     }
 
     // Discard cards
-    $cards = Cards::get($cardIds);
+    $cards = Cards::getMany($cardIds);
     Cards::discard($cardIds);
     Notifications::discardCards($player, $cards);
 
