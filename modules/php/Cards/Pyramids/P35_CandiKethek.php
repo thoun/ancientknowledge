4463ee05e36c25da48d13c4fc9fba7d24eece103
Syarwin <timothee.pecatta@ens-lyon.fr>
2023-10-05 21:24:40 +0200

    P35_CandiKethek backend

--- modules/php/Cards/Pyramids/P35_CandiKethek.php
+++ modules/php/Cards/Pyramids/P35_CandiKethek.php
@@ -1,6 +1,11 @@
 <?php
 namespace AK\Cards\Pyramids;
 
+use AK\Core\Notifications;
+use AK\Managers\Players;
+use AK\Managers\Technologies;
+use AK\Actions\Learn;
+
 class P35_CandiKethek extends \AK\Models\Building
 {
   public function __construct($row)
@@ -18,5 +23,67 @@ class P35_CandiKethek extends \AK\Models\Building
     $this->startingSpace = 2;
     $this->activation = IMMEDIATE;
     $this->effect = [clienttranslate('Choose 2 available Technologies and put them on the bottom of their respective deck(s).')];
+    $this->implemented = true;
+  }
+
+  public function getImmediateEffect()
+  {
+    return [
+      'type' => NODE_SEQ,
+      'childs' => [
+        [
+          'action' => SPECIAL_EFFECT,
+          'args' => [
+            'sourceId' => $this->id,
+            'method' => 'chooseTech',
+          ],
+        ],
+        [
+          'action' => SPECIAL_EFFECT,
+          'args' => [
+            'sourceId' => $this->id,
+            'method' => 'chooseTech',
+          ],
+        ],
+      ],
+    ];
+  }
+
+  public function getChooseTechDescription()
+  {
+    return clienttranslate('Put 1 Technology at the bottom of the corresponding deck');
+  }
+
+  public function argsChooseTech()
+  {
+    $techs = Technologies::getPool();
+
+    return [
+      'sourceId' => $this->id,
+      'description' => clienttranslate('${actplayer} must choose 1 Technology to be placed at the bottom of the deck'),
+      'descriptionmyturn' => clienttranslate('${you} must choose 1 Technology to be placed at the bottom of the deck'),
+      '_private' => [
+        'active' => [
+          'techIds' => $techs->getIds(),
+        ],
+      ],
+    ];
+  }
+
+  public function actChooseTech($techId = null)
+  {
+    $args = $this->argsChooseTech()['_private']['active'];
+    if (!in_array($techId, $args['techIds'])) {
+      throw new \BgaVisibleSystemException('Invalid tech to select. Should not happen');
+    }
+
+    $tech = Technologies::getSingle($techId);
+    $board = $tech->getBoard();
+    $level = $tech->getLevel();
+    Technologies::insertAtBottom($techId, "deck_$level");
+    Learn::refillBoardIfNeeded($board);
+
+    $player = Players::getActive();
+    Notifications::placeAtDeckBottom($player, $tech, $level);
   }
 }
