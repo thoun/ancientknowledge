45f0dba26988101f646d37c1faac3139d4e27f7e
Syarwin <timothee.pecatta@ens-lyon.fr>
2023-10-20 20:03:36 +0200

    Make CandiSukuh lamer

--- modules/php/Technologies/T8_Ptolemy.php
+++ modules/php/Technologies/T8_Ptolemy.php
@@ -19,7 +19,7 @@ class T8_Ptolemy extends \AK\Models\Technology
 
   public function canBePlayed($player)
   {
-    return $player->getHand()->count() <= 1 + $player->getIconReduction();
+    return $player->getHand()->count() <= 1 - $player->getIconReduction();
   }
 
   public function getImmediateEffect()
