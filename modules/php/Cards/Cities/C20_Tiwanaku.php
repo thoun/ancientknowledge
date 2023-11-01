7cf478d910e047d220cdbe5fee18d6a7359d93d7
Syarwin <timothee.pecatta@ens-lyon.fr>
2023-11-01 16:54:57 +0100

    102419: "Tiwanaku removed one knowledge from itself"

--- modules/php/Cards/Cities/C20_Tiwanaku.php
+++ modules/php/Cards/Cities/C20_Tiwanaku.php
@@ -26,7 +26,7 @@ class C20_Tiwanaku extends \AK\Models\Building
   {
     $cardIds = [];
     foreach ($this->getPlayer()->getTimeline() as $card) {
-      if ($card->getType() == CITY && $card->getKnowledge() > 0) {
+      if ($card->getType() == CITY && $card->getKnowledge() > 0 && $card->getId() != $this->getId()) {
         $cardIds[] = $card->getId();
       }
     }
